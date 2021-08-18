25e2c5b5818adb311efab92c2b44ee3833bd62ca
RomanAksanov <raksanov.exceedteam@gmail.com>
2021-08-18 13:11:27 +0300

    add condition on empty body

--- src/modules/controllers/task.controller.js
+++ src/modules/controllers/task.controller.js
@@ -8,27 +8,28 @@ module.exports.getAllTasks = (req, res, next) => {
 
 module.exports.createNewTasks =  (req, res, next) => {
   const task = new Task(req.body);
-  if (!req.body.text) {
-    res.send('Error! Task is empty!!');
+  if (!(req.body.hasOwnProperty('text')) || !(req.body.hasOwnProperty('isCheck'))) {   
+    res.send('Parameters were lost!!'); 
+  } else if ((!req.body.text) || (req.body.isCheck)) {
+    res.send('Error! You need input all data!!');
   } else {
     task.save().then(result => {
       Task.find().then(result => {
         res.send({data: result});
       });
     });
-  }    
+  }
 };
 
-
 module.exports.changeTaskInfo = (req, res, next) => {
-  if (!req.body._id || !req.body) {
-    res.send('Please check id or task. Maybe they are empty');
-  } else {
+  if ((req.body._id) && (req.body.text)) {
     Task.updateOne({_id: req.body._id}, req.body).then(result => {
       Task.find().then(result => {
         res.send({data: result});
       });
     });
+  } else {
+    res.send('Please check id or task. Maybe they are empty');
   }
 };
 
